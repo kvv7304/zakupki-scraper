2a720f4190c0adf2dd800f2aab675fcb4dd3b5a6
kvv7304 <139488213+kvv7304@users.noreply.github.com>
2024-06-22 17:29:14 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+key = "your_google_sheets_key"
+filename = "config"
+
+url44fz = "https://zakupki.gov.ru/epz/dizk/search/results.html?recordsPerPage=_100"
+urlDishonestsupplier = "https://zakupki.gov.ru/epz/dishonestsupplier/search/results.html?morphology=on&search-filter=Дата+размещения&sortBy=DATE_OF_INCLUSION&sortDirection=false&recordsPerPage=_100&showLotsInfoHidden=false&fz94=on&fz223=on&ppRf615=on&inclusionDateFrom=01.01.2023"
+urlPetition = "https://zakupki.gov.ru/epz/complaint/search/search_eis.html?morphology=on&search-filter=Дата+размещения&fz94=on&fz223=on&receiptDateStart=01.04.2023&sortBy=PO_DATE_POSTUPLENIYA&sortDirection=false&showLotsInfoHidden=false&recordsPerPage=_100"
+
+sbisUrl = "https://sbis.ru/contragents/"
+vbankcenterUrl = "https://vbankcenter.ru/contragent/"
+checkoUrl = "https://checko.ru/company/"
+excheckUrl = "https://excheck.pro/company/"
+listOrgUrl = "https://www.list-org.com"
+findOrgUrl = "https://www.find-org.com"
+
+bot = "https://www.find-org.com/bot.html"
+captcha_key = "your_captcha_key"
+
+proxi = {
+    'http': 'http://username:password@proxy_address:port',
+    'https': 'http://username:password@proxy_address:port'
+}
